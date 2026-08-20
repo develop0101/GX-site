c30fd3f8a183b41b4cbaaaf96b0ee29707cc5858
develop0101 <122296013+develop0101@users.noreply.github.com>
2026-08-20 08:38:29 -0400

    Load visual interaction overrides

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './visual-overrides.css';
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
